eb0ff168e2e91a91b65c45a7dbeac3096ac67f40
Michael Murphy <murphy17@mit.edu>
2017-06-28 16:32:50 -0400

    added test files

--- test/run.sh
+++ test/run.sh
@@ -4,5 +4,4 @@
 # num_frames = 0 to only process files currently in folder, -1 to monitor realtime
 # save_volume = 0 or 1
 
-rm -rf "/mnt/image_store/Peng_Lindsey/cropped/.*"
-../Debug/DHMProcessor "/mnt/image_store/Peng_Lindsey/cropped" "/mnt/image_store/Peng_Lindsey/cropped_out" 30 1 20 0 1
+../Debug/DHMProcessor "./in" "./out" 30 1 20 0 1
